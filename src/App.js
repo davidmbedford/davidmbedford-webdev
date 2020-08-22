68ae6f7677a50c7f96d2f228822a38f5a13007b0
davidmbedford <david.m.bedford1@gmail.com>
2020-08-22 00:39:25 -0400

    all js files filled, troubleshooting navbar complete, home screen is next

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Row } from 'reactstrap';
 import './App.css';
 
-import Home from './screens/home/index.js';
+// import home from './screens/Home/index.js';
 import Portfolio from './screens/portfolio/index.js';
 import Skills from './screens/skills/index.js';
 import Contact from './screens/contact/index.js';
@@ -16,7 +16,7 @@ function App() {
     <Row style={{ margin: 0 }}>
     <Navigation /> 
       <Switch>
-        <Route exact path='/' component={Home}/>
+        {/* <Route exact path='/' component={Home}/> */}
         <Route exact path='/portfolio' component={Portfolio}/>
         <Route exact path='/skills' component={Skills}/>
         <Route exact path='/contact' component={Contact}/>
